7dd8f704e04652b54e587da3846602e767563869
fuyf <fuyf@sunwayworld.com>
2021-02-26 09:28:03 +0800

    init demo

---
+++ src/router/index.ts
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import Home from '../views/home/index.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/mine',
+    name: 'Mine',
+    // route level code-splitting
+    // this generates a separate chunk (mine.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "mine" */ '../views/mine/index.vue')
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    // route level code-splitting
+    // this generates a separate chunk (mine.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "profile" */ '../views/profile/index.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
